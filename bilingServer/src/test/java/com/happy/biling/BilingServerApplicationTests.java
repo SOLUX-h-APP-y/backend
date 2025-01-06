9272fca91111f15f4c997a4f9f3f97144c1ed9e1
tishakong <leehn45@naver.com>
2025-01-07 03:20:25 +0900

    init: spring boot 프로젝트 생성 및 mysql db 연결

---
+++ bilingServer/src/test/java/com/happy/biling/BilingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.biling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
